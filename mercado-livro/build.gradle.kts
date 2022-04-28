6e2651b56a475fc9aba5ab9f79052268c285976d
Francisco Augusto Nascimento <franascimento1107@bol.com.br>
2022-04-28 06:27:20 -0300

    Terminou os fundamentos

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	application
 }
 
 group = "com.mercadolivro"
@@ -32,4 +33,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+application {
+	mainClassName = "com.mercadolivro.mercadolivro.MercadoLivroApplicationKt"
 }
