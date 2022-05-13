0afb6139a5645d4e1790eb1add0d21248283d442
Francisco Augusto Franco do Nascimento <hm4q@petrobras.com.br>
2022-05-13 12:02:35 -0300

    Funções de nova entidade

--- mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.extension
 
 import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
@@ -16,4 +17,8 @@ fun PutCustomerRequest.toCustomerModel(id: Int): CustomerModel {
 
 fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
     return BookModel(id = null, name = this.name, price = this.price, status = BookStatus.ATIVO, customer)
+}
+
+fun PutBookRequest.toBookModel(id: Int, customer: CustomerModel): BookModel {
+    return BookModel(id = id, name = this.name, price = this.price, status = BookStatus.ATIVO, customer)
 }
